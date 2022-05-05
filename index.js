da52aa6c9ea2603e289c6869c01d8c64ec2b4c68
shalu2022 <98653679+shalu2022@users.noreply.github.com>
2022-05-05 16:02:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function display(){
+    document.getElementById("box").style.display ="block";
+    document.querySelector("body").style.backgroundColor ="#E8F9FD";
+}
+
+function del(){
+    document.getElementById("box").style.display ="none";
+}
